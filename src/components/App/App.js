4091b36fcf7b20e06bd8face55aaf575e852ca0b
off2mxrs <off2mxrs@gmail.com>
2022-01-13 16:32:18 -0800

    Buiness.js rendered

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import Business from '../Buisness/Business';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Business />
+    </div>
+  );
+}
+
+export default App;
