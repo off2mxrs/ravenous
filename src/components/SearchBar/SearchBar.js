63eb8377ce3a6a0009bd38d5302a8373a7ef633d
off2mxrs <off2mxrs@gmail.com>
2022-01-17 22:09:48 -0800

    Added State and Handles

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,23 +1,70 @@
 import React from 'react'
 import './SearchBar.css'
 
-//Yelp Api sort_by parameters
-//https://www.yelp.com/developers/documentation/v3/business_search
-const sortByOptions = {
-    'Best Match': 'best_match',
-    'Highest Rated': 'rating',
-    'Most Reviewed': 'review_count'
-}
-
 class SearchBar extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            term: '',
+            location: '',
+            sortBy:'best_match',
+                     }
+        // bind both methods to the current value of this
+        this.handleTermChange.bind(this)
+        this.handleLocationChange.bind(this)
+
+        //Yelp Api sort_by parameters
+    //https://www.yelp.com/developers/documentation/v3/business_search
+        this.sortByOptions = {
+            'Best Match': 'best_match',
+            'Highest Rated': 'rating',
+            'Most Reviewed': 'review_count'
+        }
+    }
+///// CONSTRUCTOR END //////////////////////////////////
+////////////////////////////////////////////////////////////////
+
+    /// returns the current CSS class for a sorting option.
+    getSortByClass(sortByOption) {
+        if (this.sortBy === sortByOption) {
+            return 'active'
+        } else {
+            return ''
+        }
+    }
+////////// HANDLES //////////////////////////////////////////////////////////////////
+    handleSortByChange(sortByOption) {
+        this.setState(this.sortBy = {sortByOption})
+    }
+
+    handleTermChange(event) {
+        //the state of each input element should be updated to reflect the text typed into the respective input element.
+        this.setState({term: event.target.value})
+    }
+
+    handleLocationChange(event) {
+        //the state of each input element should be updated to reflect the text typed into the respective input element.
+        this.setState({term: event.target.value})
+    }
+/// HANDLES END //////////////////////////////////////////////////////////////////
+
+
     /// dynamically create the list items needed to 
     // display the sort options 
     //(Best Match, Highest Rated, Most Reviewed)//////
     renderSortByOptions() {
         //To iterate through the object, you’ll need to start by accessing the keys of the sortByOptionsobject
-        return Object.keys(sortByOptions).map(sortByOption => {
-            let sortByOptionValue = sortByOptions[sortByOption]
-            return <li key={sortByOptionValue}>{sortByOption}</li>
+        return Object.keys(this.sortByOptions).map(sortByOption => {
+            let sortByOptionValue = this.sortByOptions[sortByOption]
+
+            return <li ///This will conditionally style each sort by option, displaying to the user which sorting option is currently selected.
+                     className={this.getSortByClass(sortByOptionValue)}
+                     /// bind to the current value of this but also bind the current sortByOptionValue as the first argument to the method call, ensuring the method is called with the appropriate value when clicked.
+                     onClick={this.handleSortByChange.bind(this, sortByOptionValue)}
+                     key={sortByOptionValue}
+                   >
+                     {sortByOption}
+                   </li>
         })
     }
 
@@ -30,8 +77,8 @@ class SearchBar extends React.Component {
         </ul>
     </div>
     <div className="SearchBar-fields">
-        <input placeholder="Search Businesses" />
-        <input placeholder="Where?" />
+        <input onChange={this.handleTermChange} placeholder="Search Businesses" />
+        <input onChange={this.handleLocationChange} placeholder="Where?" />
     </div>
     <div className="SearchBar-submit">
         <a>Let's Go</a>
